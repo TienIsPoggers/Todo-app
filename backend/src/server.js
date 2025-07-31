4a43d3d893f1b428e7a43e9ed5a9ecc8d6ab4b09
TienIsPoggers <hangquoctien2008@gmail.com>
2025-07-31 21:10:32 +0700

    04 commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,10 @@ import cors from 'cors';
 import taskRoutes from './routes/taskRoutes.js'
 import path from 'path'
 import { ConnectDB } from './config/db.js';
+import { fileURLToPath } from 'url';
 dotenv.config();
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 5000;
 ConnectDB();
@@ -14,12 +16,13 @@ if(process.env.NODE_ENV !== 'production'){
         origin:"http://localhost:5173"
     }));
 }
+
 app.use(express.json());
 app.use('/api/tasks',taskRoutes)
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-    app.get('/',(req,res) => {
-        res.sendFile(path.join(__dirname,"../frontend",'dist','index.html'))
+    app.use(express.static(path.join(__dirname,"../../frontend/dist")))
+    app.get(/.*/,(req,res) => {
+        res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
     })
 }
 app.listen(PORT,() => console.log("Server started with PORT:",PORT))
